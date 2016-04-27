f658cb2012f482daa17daa9c3263e7693603c309
hnosal <h.nosal@seznam.cz>
2016-04-27 08:58:46 +0200

    new reversi
    new Game

---
+++ testgame/src/reversi/main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reversi;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class main extends JFrame{
+    
+    private static JPanel pnlLeft;
+    
+    public main() {
+        super("Reversi");
+        setLayout(new BorderLayout());
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int height = screenSize.height;
+        int width = screenSize.width;
+        setSize(width/2, height/2);
+
+        // center the jframe on screen
+        setLocationRelativeTo(null);
+        
+        pnlLeft = new ReversiGui();
+        add(pnlLeft, BorderLayout.CENTER);
+        
+        setSize(800, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new main();
+    }
+    
+}
